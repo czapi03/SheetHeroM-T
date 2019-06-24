809446ab438dfa217b545c13cd065d6aa5315e2a
czapi03 <49277551+czapi03@users.noreply.github.com>
2019-06-24 17:30:21 +0200

    tastatur onclick "0.1.0-b005"

--- www/js/app.js
+++ www/js/app.js
@@ -9,8 +9,11 @@ document.addEventListener('deviceready', function() {
     console.log($(window).width(), $(window).height());
 
     // x,y,width,height,color
-    var rect1 = new Background(0, 0, $(window).width(), $(window).height(), "#a09d9a");
+    // var rect1 = new Background(0, 0, $(window).width(), $(window).height(), "#a09d9a");
 
+    $('defs > path').on("click",function(){
+      $(this).toggleClass('blue')
+    })
 
 
   })
