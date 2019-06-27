aad162de45d021f6c060d92df35c5c404d583983
czapi03 <49277551+czapi03@users.noreply.github.com>
2019-06-27 18:16:13 +0200

    zurück ohne plugins

--- www/js/game/pianoOnClick.js
+++ www/js/game/pianoOnClick.js
@@ -2,13 +2,12 @@ class PianoOnClick {
   constructor(){
     this.allkeys = $('.pianoOnClick').on('click',function(){
 
-
       var mykey = $(this).attr('id');
       console.log(mykey);
 
-      var media = new Media('../../assets/wav/'+ mykey +'.wav');
-      media.play();
-      console.log(media);
+      var audio = new Audio('../../assets/wav/'+ mykey +'.wav');
+      audio.play();
+      console.log(audio);
 
 
 
