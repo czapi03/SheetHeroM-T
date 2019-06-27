8e53b632fc3cf7f8ca84b410cd1300077923d26b
czapi03 <49277551+czapi03@users.noreply.github.com>
2019-06-27 15:58:57 +0200

    some

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,21 @@
 //some code
+// show error on mobile
+window.onerror = function( err ) {
+  console.log( err );
+  return true;
+}
+console.log = function(s) {
+    $( '#debug').html(s).css({padding:10});
+}
 document.addEventListener('deviceready', function() {
 
 
   $('document').ready(function() {
     console.log('app.js');
 
-    window.onerror = function( err ) {
-      console.log( err );
-      return true;
-    }
-    console.log = function(s) {
-      $( '#debug').html(s).css({padding:10});
-    }
 
-    console.log($(window).width(), $(window).height());
+
+
 
     // x,y,width,height,color
     // var rect1 = new Background(0, 0, $(window).width(), $(window).height(), "#a09d9a");
