5c776c54a57277cb731d9f9f33f12a849bf80eae
czapi03 <49277551+czapi03@users.noreply.github.com>
2019-06-27 15:28:06 +0200

    test console log

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,11 @@
 //some code
-
+window.onerror = function( err ) {
+  console.log( err );
+  return true;
+}
+console.log = function(s) {
+    $( '#debug').html(s).css({padding:10});
+}
 document.addEventListener('deviceready', function() {
 
 
