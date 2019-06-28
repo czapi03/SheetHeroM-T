8a3b188582cad18e14afef3af8bc4de91ff42393
czapi03 <49277551+czapi03@users.noreply.github.com>
2019-06-28 16:09:27 +0200

    v39

--- www/js/game/pianoOnClick.js
+++ www/js/game/pianoOnClick.js
@@ -1,11 +1,15 @@
 class PianoOnClick {
-  constructor(){
-    this.allkeys = $('.pianoOnClick').on('click',function(){
+  constructor() {
+    this.allkeys = $('.pianoOnClick').on('click', function() {
 
       var mykey = $(this).attr('id');
       console.log(mykey);
 
-      var media = new Media('../../assets/wav/'+ mykey +'.wav',function(){console.log('geht');},function(err){console.log(err);});
+      var media = new Media('../../assets/wav/' + mykey + '.wav', function() {
+        console.log('geht');
+      }, function(err) {
+        console.log(err);
+      });
       media.play();
       console.log(media.src);
 
