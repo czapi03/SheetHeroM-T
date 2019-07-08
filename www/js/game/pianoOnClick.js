2c33aad513d168b1bc7eda8b85ee9ea38a3ed8b0
czapi03 <49277551+czapi03@users.noreply.github.com>
2019-07-08 21:30:21 +0200

    Update pianoOnClick.js

--- www/js/game/pianoOnClick.js
+++ www/js/game/pianoOnClick.js
@@ -6,23 +6,23 @@ class PianoOnClick {
 
       $('#game').trigger('keyhit',mykey)
 
-      
 
 
 
 
 
-      // // var media = new Media('assets/wav/' + mykey + '.wav', function() {
-      // var media = new Media("/android_asset/www/assets/wav/"+ mykey +".wav", function() {
-      //
-      //     media.release();
-      //
-      // }, function(err) {
-      //
-      //   alert(JSON.stringify(err));
-      //
-      // });
-      // media.play();
+
+      // var media = new Media('assets/wav/' + mykey + '.wav', function() {
+      var media = new Media("/android_asset/www/assets/wav/"+ mykey +".wav", function() {
+
+          media.release();
+
+      }, function(err) {
+
+        alert(JSON.stringify(err));
+
+      });
+      media.play();
 
 
 
