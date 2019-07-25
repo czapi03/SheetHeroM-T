523af156f7884a1ed789a7ee47f03894c6886a31
czapi03 <49277551+czapi03@users.noreply.github.com>
2019-07-25 15:07:05 +0200

    midi piano final "Fixes #23 "

--- www/js/game/gamelogic.js
+++ www/js/game/gamelogic.js
@@ -15,6 +15,7 @@ class Game {
     this.keyhitfunction(speed);
     this.media;
     this.mediaarray;
+    this.midiinput;
     this.self = this;
     this.move;
     this.progressbarupdate = 0;
@@ -40,7 +41,7 @@ class Game {
     //mutebutton
 
     this.lvlloader();
-    this.midipiano();
+    this.midipiano2();
 
 
     this.pianoOnClick = $('.pianoOnClick').on('click', function() {
@@ -291,6 +292,8 @@ class Game {
 
 
   stop(){
+    this.midiinput.removeListener("noteon");
+    WebMidi.disable();
     $('#game').off("keyhit")
     $('.refreshbutton').off("click")
      $('#mutebutton').off("click")
@@ -314,140 +317,263 @@ class Game {
   }
 
 
-  midipiano(){
+  // midipiano(){
+  //
+  //       var mykey= 0;
+  //
+  //         if (navigator.requestMIDIAccess) {
+  //           console.log('This browser supports WebMIDI!');
+  //         } else {
+  //           console.log('WebMIDI is not supported in this browser.');
+  //         }
+  //
+  //         navigator.requestMIDIAccess()
+  //         .then(onMIDISuccess, onMIDIFailure)
+  //         .then(function(sd){
+  //           console.log(sd);
+  //         })
+  //
+  //
+  //         function onMIDIFailure() {
+  //           console.log('Could not access your MIDI devices.');
+  //         }
+  //
+  //         function onMIDISuccess(midiAccess) {
+  //           for (var input of midiAccess.inputs.values()){
+  //             input.onmidimessage = getMIDIMessage;
+  //             console.log(midiAccess);
+  //           }
+  //         }
+  //
+  //         function getMIDIMessage(midiMessage) {
+  //
+  //           console.log(midiMessage);
+  //           // console.log(testdsd);
+  //           switch (midiMessage.data[1]) {
+  //             case 60: mykey = "c4"
+  //
+  //               break;
+  //             case 61: mykey = "csharpdflat4"
+  //
+  //               break;
+  //             case 62: mykey = "d4"
+  //
+  //               break;
+  //             case 63: mykey = "dsharpeflat4"
+  //
+  //               break;
+  //             case 64: mykey = "e4"
+  //
+  //               break;
+  //             case 65: mykey = "f4"
+  //
+  //               break;
+  //             case 66: mykey = "fsharpgflat4"
+  //
+  //               break;
+  //             case 67: mykey = "g4"
+  //
+  //               break;
+  //             case 68: mykey = "gsharpaflat4"
+  //
+  //               break;
+  //             case 69: mykey = "a4"
+  //
+  //               break;
+  //             case 70: mykey = "asharpbflat4"
+  //
+  //               break;
+  //             case 71: mykey = "h4"
+  //
+  //               break;
+  //             case 72: mykey = "c5"
+  //
+  //               break;
+  //             case 73: mykey = "csharpdflat5"
+  //
+  //               break;
+  //             case 74: mykey = "d5"
+  //
+  //               break;
+  //             case 75: mykey = "dsharpeflat5"
+  //
+  //               break;
+  //             case 76: mykey = "e5"
+  //
+  //               break;
+  //             case 77: mykey = "f5"
+  //
+  //               break;
+  //             case 78: mykey = "fsharpgflat5"
+  //
+  //               break;
+  //             case 79: mykey = "g5"
+  //
+  //               break;
+  //             case 80: mykey = "gsharpaflat5"
+  //
+  //               break;
+  //             case 81: mykey = "a5"
+  //
+  //               break;
+  //             case 82: mykey = "asharpbflat5"
+  //
+  //               break;
+  //             case 83: mykey = "h5"
+  //
+  //               break;
+  //             case 84: mykey = "c6"
+  //
+  //               break;
+  //
+  //
+  //             default: console.log("nicht die taste");
+  //
+  //               break;
+  //
+  //           }
+  //           if($('#audios').hasClass("muteall")){
+  //             console.log("muted");
+  //             var media = $("<audio>").attr({class:"playing"}).append('<source src="./assets/wav/' + mykey + '.wav" type="audio/ogg" />').appendTo("#audios");
+  //           }else{
+  //
+  //             var media = $("<audio>").attr({"autoplay":"true",class:"playing"}).append('<source src="./assets/wav/' + mykey + '.wav" type="audio/ogg" />').appendTo("#audios");
+  //           }
+  //           // console.log(media);
+  //
+  //           setTimeout(function () {
+  //             media.remove()
+  //           }, 1500);
+  //
+  //           $('#game').trigger('keyhit', mykey)
+  //           return mykey;
+  //
+  //         }
+  //
+  //
+  //
+  //
+  //
+  // }
 
-        var mykey= 0;
+  midipiano2(){
 
-          if (navigator.requestMIDIAccess) {
-            console.log('This browser supports WebMIDI!');
-          } else {
-            console.log('WebMIDI is not supported in this browser.');
-          }
+    var mykey;
 
-          navigator.requestMIDIAccess()
-          .then(onMIDISuccess, onMIDIFailure)
-          .then(function(sd){
-            console.log(sd);
-          })
+    WebMidi.enable(function () {
 
+      this.midiinput = WebMidi.getInputByName("microKEY-37");
 
-          function onMIDIFailure() {
-            console.log('Could not access your MIDI devices.');
-          }
+      this.midiinput.addListener('noteon', 'all',
+        function (e) {
+            console.log("Received 'noteon' message (" + e.note.name + e.note.octave + ").");
+            switch (e.note.name + e.note.octave) {
+                  case "C4": mykey = "c4"
 
-          function onMIDISuccess(midiAccess) {
-            for (var input of midiAccess.inputs.values()){
-              input.onmidimessage = getMIDIMessage;
-              console.log(midiAccess);
-            }
-          }
+                    break;
+                  case "C#4": mykey = "csharpdflat4"
 
-          function getMIDIMessage(midiMessage) {
+                    break;
+                  case "D4": mykey = "d4"
 
-            console.log(midiMessage);
-            // console.log(testdsd);
-            switch (midiMessage.data[1]) {
-              case 60: mykey = "c4"
+                    break;
+                  case "D#4": mykey = "dsharpeflat4"
 
-                break;
-              case 61: mykey = "csharpdflat4"
+                    break;
+                  case "E4": mykey = "e4"
 
-                break;
-              case 62: mykey = "d4"
+                    break;
+                  case "F4": mykey = "f4"
 
-                break;
-              case 63: mykey = "dsharpeflat4"
+                    break;
+                  case "F#4": mykey = "fsharpgflat4"
 
-                break;
-              case 64: mykey = "e4"
+                    break;
+                  case "G4": mykey = "g4"
 
-                break;
-              case 65: mykey = "f4"
+                    break;
+                  case "G#4": mykey = "gsharpaflat4"
 
-                break;
-              case 66: mykey = "fsharpgflat4"
+                    break;
+                  case "A4": mykey = "a4"
+                    break;
+                  case "A#4": mykey = "asharpbflat4"
+                  break;
+                  case "B4": mykey = "h4"
+                  break;
+                  case "C5": mykey = "c5"
 
-                break;
-              case 67: mykey = "g4"
+                    break;
+                  case "C#5": mykey = "csharpdflat5"
 
-                break;
-              case 68: mykey = "gsharpaflat4"
+                    break;
+                  case "D5": mykey = "d5"
 
-                break;
-              case 69: mykey = "a4"
+                    break;
+                  case "D#5": mykey = "dsharpeflat5"
 
-                break;
-              case 70: mykey = "asharpbflat4"
+                    break;
+                  case "E5": mykey = "e5"
 
-                break;
-              case 71: mykey = "h4"
+                    break;
+                  case "F5": mykey = "f5"
 
-                break;
-              case 72: mykey = "c5"
+                    break;
+                  case "F#5": mykey = "fsharpgflat5"
 
-                break;
-              case 73: mykey = "csharpdflat5"
+                    break;
+                  case "G5": mykey = "g5"
 
-                break;
-              case 74: mykey = "d5"
+                    break;
+                  case "G#5": mykey = "gsharpaflat5"
 
-                break;
-              case 75: mykey = "dsharpeflat5"
+                    break;
+                  case "A5": mykey = "a5"
+                    break;
+                  case "A#5": mykey = "asharpbflat5"
+                  break;
+                  case "B5": mykey = "h5"
+                  break;
+                  case "C6": mykey = "c6"
+                  break;
 
-                break;
-              case 76: mykey = "e5"
+                  default:
+                  console.log("not se right key");
+                  mykey = 0;
 
-                break;
-              case 77: mykey = "f5"
-
-                break;
-              case 78: mykey = "fsharpgflat5"
-
-                break;
-              case 79: mykey = "g5"
-
-                break;
-              case 80: mykey = "gsharpaflat5"
-
-                break;
-              case 81: mykey = "a5"
-
-                break;
-              case 82: mykey = "asharpbflat5"
-
-                break;
-              case 83: mykey = "h5"
-
-                break;
-              case 84: mykey = "c6"
-
-                break;
+                  break;
+                }//switch
+                    if(typeof mykey === "string"){
 
 
-              default: console.log("nicht die taste");
+                      if($('#audios').hasClass("muteall")){
+                        console.log("muted");
+                        var media = $("<audio>").attr({class:"playing"}).append('<source src="./assets/wav/' + mykey + '.wav" type="audio/ogg" />').appendTo("#audios");
+                      }else{
 
-                break;
+                        var media = $("<audio>").attr({"autoplay":"true",class:"playing"}).append('<source src="./assets/wav/' + mykey + '.wav" type="audio/ogg" />').appendTo("#audios");
+                      }
+                      // console.log(media);
 
-            }
-            if($('#audios').hasClass("muteall")){
-              console.log("muted");
-              var media = $("<audio>").attr({class:"playing"}).append('<source src="./assets/wav/' + mykey + '.wav" type="audio/ogg" />').appendTo("#audios");
-            }else{
+                      setTimeout(function () {
+                        media.remove()
+                      }, 1500);
 
-              var media = $("<audio>").attr({"autoplay":"true",class:"playing"}).append('<source src="./assets/wav/' + mykey + '.wav" type="audio/ogg" />').appendTo("#audios");
-            }
-            // console.log(media);
-
-            setTimeout(function () {
-              media.remove()
-            }, 1500);
-
-            $('#game').trigger('keyhit', mykey)
-            return mykey;
-
-          }
+                      $('#game').trigger('keyhit', mykey)
+                    }
 
 
+
+
+        }
+    );
+
+
+
+
+
+
+  }.bind(this))
 
 
 
