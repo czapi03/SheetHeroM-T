38f1111c1bbf8f9f0e9287096f7d919ed6014557
czapi03 <49277551+czapi03@users.noreply.github.com>
2019-08-13 10:47:15 +0200

    final1.1

--- www/js/jsdoku.js
+++ www/js/jsdoku.js
@@ -11,23 +11,3 @@ $.svg = function( tag ) {
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
-
-//console log mobile
-// window.onerror = function(err) {
-//   console.log(err);
-//   return true;
-// }
-// console.log = function(s) {
-//   $("<div>").html(s).css({
-//     padding: 5,
-//     "background": "#e57b7b"
-//   }).appendTo('#debug')
-// }
-
-//Nächstes LVl generieren
-// function generateNextLevel(currentlvl,noten){
-//   $('#dialog').css({display:"none"})
-//
-//   var startnextlvl = new Game(currentlvl, noten, 0.50, 60, 0,1000, "Anfänger", 150)
-//
-// }
